7fe879419fbff92d7d852686da071479b2cdb207
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-04-26 00:49:48 +0300

    initial commit classes

---
+++ src/test/java/com/cydeo/tests/day01_selenium_intro/SeleniumTest.java
@@ -0,0 +1,20 @@
+package com.cydeo.tests.day01_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+
+        // 1- Setting up the web driver manager
+        WebDriverManager.chromedriver().setup(); //  --> ilgili class'ı import et
+
+        // 2- Create instance of the chrome driver obj.
+        WebDriver driver=new ChromeDriver();
+
+        // 3- Test if driver is working as expected
+        driver.get("https://www.google.com.tr");
+    }
+}
