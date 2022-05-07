fce89473ef8eca49b01e6fa2e9a1779dcf5ca541
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-07 16:33:25 +0300

    day02 task1 Cydeo Verification

---
+++ src/test/java/com/cydeo/tests/day02_locaters_getText_getAttribute/TC001_CydeoVerifications.java
@@ -0,0 +1,18 @@
+package com.cydeo.tests.day02_locaters_getText_getAttribute;
+
+public class TC001_CydeoVerifications {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+/*
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com
+3. Verify URL contains
+Expected: cydeo
+4. Verify title:
+ Expected: Practice
+ */
