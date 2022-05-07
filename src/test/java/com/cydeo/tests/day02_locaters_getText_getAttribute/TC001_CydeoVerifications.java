5b459d2c785b9fdfb13e5d501b8b8ea1a289a1c5
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-07 16:43:16 +0300

    day02 task1 Cydeo Verification

--- src/test/java/com/cydeo/tests/day02_locaters_getText_getAttribute/TC001_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day02_locaters_getText_getAttribute/TC001_CydeoVerifications.java
@@ -1,18 +1,43 @@
 package com.cydeo.tests.day02_locaters_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC001_CydeoVerifications {
 
     public static void main(String[] args) {
+
+        // 1. Open Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Go to https://practice.cydeo.com
+        driver.navigate().to("https://practice.cydeo.com");
+
+        // 3. Verify URL contains
+        // Expected: cydeo
+        String expectedURL="cydeo";
+        String currentURL=driver.getCurrentUrl();
+
+        if (expectedURL.contains(currentURL)){
+            System.out.println("URL verification PASSED!");
+        }else{
+            System.out.println("URL verification FAILED!");
+        }
+
+
+
+// 4. Verify title:
+//  Expected: Practice
+
+
 
 
 
     }
 }
-/*
-1. Open Chrome browser
-2. Go to https://practice.cydeo.com
-3. Verify URL contains
-Expected: cydeo
-4. Verify title:
- Expected: Practice
- */
+
+
