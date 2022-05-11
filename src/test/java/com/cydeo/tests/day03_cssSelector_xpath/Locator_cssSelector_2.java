b369645bae4eb0ec14946952ee78c6ba39cf163b
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-11 22:24:03 +0300

    day03 Task4 completed

--- src/test/java/com/cydeo/tests/day03_cssSelector_xpath/Locator_cssSelector_2.java
+++ src/test/java/com/cydeo/tests/day03_cssSelector_xpath/Locator_cssSelector_2.java
@@ -1,18 +1,35 @@
 package com.cydeo.tests.day03_cssSelector_xpath;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Locator_cssSelector_2 {
 
     public static void main(String[] args) {
 
         //TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a chrome browser
-
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.navigate().to("https://login1.nextbasecrm.com/?forgot_password=yes");
 
 
         //3- Verify “Reset password” button text is as expected:
         //Expected: Reset password
+        WebElement resetPasswordButton=driver.findElement(By.cssSelector("button.login-btn"));
+        String expectedResetPasswordButtonText="Reset password";
+        String actualResetPasswordButtonText=resetPasswordButton.getText();
+
+        if (actualResetPasswordButtonText.equals(expectedResetPasswordButtonText)){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+
+        }
 
 
         //PS: Inspect and decide which locator you should be using to locate web elements.
