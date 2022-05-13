ae296f16b564c4991cfb736f861208ea4f00df08
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-13 14:47:47 +0300

    day04 Task6 Exception completed

---
+++ src/test/java/com/cydeo/tests/day04_checkBox_radioButton/TC06_StaleElementReferenceException.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day04_checkBox_radioButton;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class TC06_StaleElementReferenceException {
+
+    public static void main(String[] args) {
+
+        //TC #6: StaleElementReferenceException Task
+        //1- Open a chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink=driver.findElement(By.xpath("//a[.='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+
+        //4- Refresh the page.
+        driver.navigate().refresh();
+
+        // We are refreshing the web element reference by re-assigning the web element
+        cydeoLink=driver.findElement(By.xpath("//a[.='CYDEO']"));
+
+        //5- Verify it is displayed, again.
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+
+
+        //This is a simple StaleElementReferenceException to understand what is
+        //this exception and how to handle it.
+
+        driver.close();
+
+
+    }
+}
