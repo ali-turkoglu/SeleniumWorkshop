754672a84606535f8890f94c92398737fa2aa83e
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-05-27 16:14:20 +0300

    day08 .properties file

---
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/LearningProperties.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests.day08_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class LearningProperties {
+
+    @Test
+    public void java_properties_reading_test(){
+
+        // key = value
+        // os.name = Windows 10
+        // user.name = alitu
+
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
+
+
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/ReadingProperties.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day08_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_test() throws IOException {
+
+        Properties properties= new Properties();
+
+        FileInputStream file=new FileInputStream("configuration.properties");
+
+        properties.load(file);
+
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"username\") = " + properties.getProperty("username"));
+
+    }
+}
