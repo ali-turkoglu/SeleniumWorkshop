516721cd648a90aca9bff632e9cd9166126cf3de
alitu <aliturkoglu@gmail.com>
2022-06-10 14:31:17 +0300

    day13 BrowserUtils explicitwait method

--- src/test/java/com/cydeo/tests/day13_review/T2_Explicit_wait_test.java
+++ src/test/java/com/cydeo/tests/day13_review/T2_Explicit_wait_test.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day13_review;
 
 import com.cydeo.pages.DynamicControls;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -8,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class T2_Explicit_wait_test {
 
@@ -25,9 +28,12 @@ public class T2_Explicit_wait_test {
         dynamicControls.removeButton.click();
 
         //4- Wait until “loading bar disappears”
+       /* Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
 
         wait.until(ExpectedConditions.invisibilityOf(dynamicControls.loadingBar));
+*/
+        BrowserUtils.waitForInvisibilityOf(dynamicControls.loadingBar);
 
         //5- Verify:
         //a. Checkbox is not displayed
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,9 +1,13 @@
 package com.cydeo.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class BrowserUtils {
 
@@ -45,6 +49,16 @@ public class BrowserUtils {
     // This method accepts a String "expectedTitle" and Asserts if it is true
     public static void verifyTitle(WebDriver driver,String expectedTitle){
         Assert.assertEquals(driver.getTitle(),expectedTitle);
+    }
+
+
+    // Creating a utility method for ExplicitWait, so we do not have to repeat the lines
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+
+
     }
 
 
